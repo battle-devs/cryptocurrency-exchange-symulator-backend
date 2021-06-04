a5a59c74b9d919541f9e2ec84e833c48a61404d2
Przemysław Krzempek <przemyslaw.krzempek@capsilon.com>
2021-06-04 13:41:01 +0200

    [EFI-28] Added Currency entity

--- src/main/java/main/entity/Currency.java
+++ src/main/java/main/entity/Currency.java
@@ -1,33 +1,33 @@
 package main.entity;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Table(name = "Assets")
+@Table(name = "Currencies")
 @Entity
 @NoArgsConstructor
 @Data
-public class Asset {
+public class Currency {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
-
-	@ManyToOne
-	private User name;
-
 	@OneToOne
-	private Currency currency;
+	private Asset asset;
+	@Column(name = "name", nullable = false)
+	private String name;
+	@Column(name = "purchase_date", nullable = false)
+	private Date dateOfPurchase;
 
 }
