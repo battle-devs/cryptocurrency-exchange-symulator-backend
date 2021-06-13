0be8f9928e26b6782a3ff8bd04f1e4e048581d5d
Michał Salamon <ms055452@student.ath.edu.pl>
2021-06-13 23:55:51 +0200

    added base external API controller

--- src/main/java/main/CryptoApp.java
+++ src/main/java/main/CryptoApp.java
@@ -1,12 +1,18 @@
 package main;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import main.services.CryptowatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CryptoApp {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(CryptoApp.class, args);
+		CryptowatClient cryptowatClient = new CryptowatClient();
+		var bub = cryptowatClient.getAssets();
+		System.out.println(new ObjectMapper().writeValueAsString(bub));
 	}
 }
--- src/main/java/main/entity/fromAPI/APIAsset.java
+++ src/main/java/main/entity/fromAPI/APIAsset.java
@@ -1,31 +1,11 @@
 package main.entity.fromAPI;
 
+import lombok.Data;
+
+@Data
 public class APIAsset {
-    private String id;
+    private Integer id;
     private String symbol;
     private String name;
     private Boolean fiat;
-
-    public APIAsset(String id, String symbol, String name, Boolean fiat) {
-        this.id = id;
-        this.symbol = symbol;
-        this.name = name;
-        this.fiat = fiat;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Boolean getFiat() {
-        return fiat;
-    }
 }
