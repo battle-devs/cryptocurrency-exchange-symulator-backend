d16c58b604badb00b12507c5102dc7f85d6966e4
Przemysław Krzempek <przemyslaw.krzempek@capsilon.com>
2021-06-17 19:00:07 +0200

    [EFI-29] in progress...

--- src/main/java/main/controller/CryptoController.java
+++ src/main/java/main/controller/CryptoController.java
@@ -1,22 +1,11 @@
 package main.controller;
 
 import lombok.AllArgsConstructor;
-import main.entity.fromAPI.APIAsset;
-import main.service.impl.CryptowatServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class CryptoController {
 
-    private CryptowatServiceImpl client;
-
-    @GetMapping("/getAssets")
-    public List<APIAsset> getAssets(){
-        return client.getAssets().getResult();
-    }
 
 }
