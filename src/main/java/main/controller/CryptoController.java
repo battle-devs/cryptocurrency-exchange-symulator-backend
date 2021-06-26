54ca0f38cd762854f77a3a6cec2e8bbd6520a944
Michał Salamon <ms055452@student.ath.edu.pl>
2021-06-26 16:03:18 +0200

    Added asset price endpoint

--- src/main/java/main/controller/CryptoController.java
+++ src/main/java/main/controller/CryptoController.java
@@ -1,12 +1,15 @@
 package main.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import main.entity.fromAPI.APIAsset;
+import main.model.APIAsset;
+import main.model.PriceRequest;
 import main.service.CryptowatService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +20,14 @@ public class CryptoController {
 
     @GetMapping("/getAssets")
     @ApiOperation("Pobierz wszystkie assety")
-    public List<APIAsset> getAssets(){
+    public List<APIAsset> getAssets() {
         return cryptowatService.getAssets().getResult();
     }
+
+    @GetMapping("/getPrice")
+    @ApiOperation("Pobiera kurs waluty")
+    public BigDecimal getPrice(@RequestBody PriceRequest priceRequest) {
+        return cryptowatService.getPrice(priceRequest);
+    }
+
 }
